18e8508ec9b7c470ab26ec643d923c20d193690c
iamrajsingh <divyraj65407@gmail.com>
2025-08-24 18:20:02 +0530

    UI Updates

---
+++ client/app/dca/page.jsx
@@ -0,0 +1,10 @@
+import DCATemplate from '@/modules/dca/template'
+import React from 'react'
+
+const Dca = () => {
+  return (
+    <DCATemplate />
+  )
+}
+
+export default Dca
---
+++ client/app/limit-orders/page.jsx
@@ -0,0 +1,10 @@
+import LimitOrderTemplate from '@/modules/limitOrders/template'
+import React from 'react'
+
+const LimitOrder = () => {
+  return (
+   <LimitOrderTemplate />
+  )
+}
+
+export default LimitOrder
